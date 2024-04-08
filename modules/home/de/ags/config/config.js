ee0b4f88673c6a81b1da2dfa86719e234aec293d
alutiamez <65709693+alutiamez@users.noreply.github.com>
2024-04-07 23:48:42 -0400

    Make ags run on hyprland start up

--- modules/home/de/ags/config/config.js
+++ modules/home/de/ags/config/config.js
@@ -2,18 +2,16 @@ const entry = `${App.configDir}/main.ts`;
 const style = `${App.configDir}/style.scss`;
 const outdir = '/tmp/ags/';
 
-
 try {
   await Utils.execAsync([
     'bun', 'build', entry,
     '--outdir', outdir,
     '--external', 'resource://*',
     '--external', 'gi://*',
-  ]);
-  await import(`file://${outdir}/js/main.js`);
-  Utils.exec(`sass ${style} ${outdir}/css/style.css`)
-  App.applyCss(`${outdir}/css/style.css`)
-  console.log("Loaded config");
+  ]); 
+  await import(`file://${outdir}/main.js`);
+  Utils.exec(`sass ${style} ${outdir}/style.css`);
+  App.applyCss(`${outdir}/style.css`);
 } catch (error) {
   console.error(error);
 };
